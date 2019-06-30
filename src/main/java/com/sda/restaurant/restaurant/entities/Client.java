1f300db7a99763e35bee6af657261d78b248791b
Jakub Terentowicz <jakub.terentowicz@icloud.com>
2019-06-30 15:17:40 +0200

    Changed ClientRepository class to Interface.

--- src/main/java/com/sda/restaurant/restaurant/entities/Client.java
+++ src/main/java/com/sda/restaurant/restaurant/entities/Client.java
@@ -1,9 +1,9 @@
 package com.sda.restaurant.restaurant.entities;
 
-import org.springframework.data.annotation.Id;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
 @Entity
 public class Client {
