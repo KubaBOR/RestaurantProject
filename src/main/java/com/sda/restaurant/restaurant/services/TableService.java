6838a18326b394cd7319d74a75cca4255c40ecb6
Jakub Terentowicz <jakub.terentowicz@icloud.com>
2019-07-04 10:46:52 +0200

    Modified table style.

---
+++ src/main/java/com/sda/restaurant/restaurant/services/TableService.java
@@ -0,0 +1,40 @@
+package com.sda.restaurant.restaurant.services;
+
+import com.sda.restaurant.restaurant.entities.Tables;
+import com.sda.restaurant.restaurant.repositories.TableRepository;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class TableService {
+
+    private final TableRepository tableRepository;
+
+    private final ModelMapper modelMapper;
+
+    @Autowired
+    public TableService(TableRepository tableRepository, ModelMapper modelMapper) {
+        this.tableRepository = tableRepository;
+        this.modelMapper = modelMapper;
+    }
+
+    public Long saveTable(Tables tables) {
+        Tables tablesToSave = modelMapper.map(tables, Tables.class);
+
+        return tableRepository.save(tablesToSave).getId();
+    }
+
+    public List<Tables> getAllTables(){
+        return tableRepository.findAll(Sort.by(Sort.Direction.ASC, "size")).stream()
+                .map(tables -> modelMapper.map(tables, Tables.class))
+                .collect(Collectors.toList());
+    }
+    public void deleteTableById(Long id){
+        tableRepository.deleteById(id);
+    }
+}
