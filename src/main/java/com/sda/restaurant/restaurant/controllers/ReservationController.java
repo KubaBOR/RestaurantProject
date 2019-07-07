d38d3d03ced7237685d2e1cdd15ea95646383603
Jakub <jakub.terentowicz@gmail.com>
2019-07-07 16:21:34 +0200

    Added method to set isPaid = true in Reservation.

--- src/main/java/com/sda/restaurant/restaurant/controllers/ReservationController.java
+++ src/main/java/com/sda/restaurant/restaurant/controllers/ReservationController.java
@@ -1,33 +1,38 @@
 package com.sda.restaurant.restaurant.controllers;
 
 import com.sda.restaurant.restaurant.DTO.ReservationDTO;
+import com.sda.restaurant.restaurant.model.ReservationEntity;
+import com.sda.restaurant.restaurant.repositories.ReservationRepository;
 import com.sda.restaurant.restaurant.services.ClientService;
 import com.sda.restaurant.restaurant.services.ReservationService;
 import com.sda.restaurant.restaurant.services.TableService;
+import javassist.NotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.view.RedirectView;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class ReservationController {
 
     private ReservationService reservationService;
 
+    private ReservationRepository reservationRepository;
+
     private ClientService clientService;
 
     private TableService tableService;
 
     @Autowired
-    public ReservationController(ReservationService reservationService, ClientService clientService, TableService tableService) {
+    public ReservationController(ReservationService reservationService, ReservationRepository reservationRepository, ClientService clientService, TableService tableService) {
         this.reservationService = reservationService;
+        this.reservationRepository = reservationRepository;
         this.clientService = clientService;
         this.tableService = tableService;
     }
@@ -37,17 +42,34 @@ public class ReservationController {
         setupModel(model);
         return "allReservationsPage";
     }
+
     @PostMapping("/addReservationAction")
-    public RedirectView addNewReservation(@ModelAttribute("reservationForm") ReservationForm reservationForm, Model model){
-
-
+    public RedirectView addNewReservation(@ModelAttribute("reservationForm") ReservationForm reservationForm, Model model) {
 
         reservationService.saveReservation(reservationForm);
         RedirectView redirectView = new RedirectView();
         redirectView.setUrl("/allReservationsPage");
         return redirectView;
     }
-    private void setupModel(Model model){
+
+    @GetMapping("/reservations/{id}")
+    public ReservationEntity retrieveReservation(@PathVariable Long id) throws NotFoundException {
+        Optional<ReservationEntity> reservation = reservationRepository.findById(id);
+        if (!reservation.isPresent()) {
+            throw new NotFoundException("id- " + id);
+        }
+        return reservation.get();
+    }
+
+    @PutMapping("/reservations/{id}")
+    public RedirectView updateReservation(ReservationDTO reservation, @PathVariable Long id) {
+        reservation = reservationService.updateStuff(id);
+        RedirectView redirectView = new RedirectView();
+        redirectView.setUrl("/allReservationsPage");
+        return redirectView;
+    }
+
+    private void setupModel(Model model) {
         List<ReservationDTO> allReservations = reservationService.getAllReservations();
         model.addAttribute("allReservations", allReservations);
         model.addAttribute("allClients", clientService.getAllClients());
@@ -55,7 +77,7 @@ public class ReservationController {
         model.addAttribute("reservationForm", new ReservationForm());
     }
 
-    public static class ReservationForm{
+    public static class ReservationForm {
         @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
         private LocalDateTime dateAndTime;
         private Boolean isPaid = false;
@@ -64,7 +86,8 @@ public class ReservationController {
         Long clientId;
         Long tablesId;
 
-        public ReservationForm(){}
+        public ReservationForm() {
+        }
 
         public Long getClientId() {
             return clientId;
