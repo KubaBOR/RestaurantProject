085efcf0674ef9c9f4e1c2e500817c5ab2de8fbe
Barbas <barbas@onet.eu>
2019-07-08 17:50:18 +0200

    added OrderController

---
+++ src/main/java/com/sda/restaurant/restaurant/controllers/OrderController.java
@@ -0,0 +1,70 @@
+package com.sda.restaurant.restaurant.controllers;
+
+import com.sda.restaurant.restaurant.DTO.OrderDTO;
+import com.sda.restaurant.restaurant.DTO.ReservationDTO;
+import com.sda.restaurant.restaurant.repositories.OrderRepository;
+import com.sda.restaurant.restaurant.services.MenuService;
+import com.sda.restaurant.restaurant.services.OrderService;
+import com.sda.restaurant.restaurant.services.ReservationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.view.RedirectView;
+
+import java.util.List;
+
+@Controller
+public class OrderController {
+    private OrderService orderService;
+    private OrderRepository orderRepository;
+    private ReservationService reservationService;
+    private MenuService menuService;
+
+    @Autowired
+    public OrderController(OrderService orderService, OrderRepository orderRepository, ReservationService reservationService, MenuService menuService) {
+        this.orderService = orderService;
+        this.orderRepository = orderRepository;
+        this.reservationService = reservationService;
+        this.menuService = menuService;
+    }
+
+    @GetMapping("/allOrdersPage")
+    public String displayAllOrders(Model model){
+        setupModel(model);
+        return "allOrdersPage";
+    }
+
+    @PostMapping("/addOrderAction")
+    public RedirectView addNewOrder(@ModelAttribute("orderDTO") OrderDTO orderDTO, Model model){
+        orderService.addOrder(orderDTO);
+        RedirectView redirectView = new RedirectView();
+        redirectView.setUrl("/allOrdersPage");
+        return redirectView;
+    }
+
+    @DeleteMapping("/deleteOrderAction/{id}")
+    public RedirectView deleteOrder(@PathVariable Long id){
+        orderService.deleteOrderById(id);
+        RedirectView redirectView = new RedirectView();
+        redirectView.setUrl("/allOrdersPage");
+        return redirectView;
+    }
+
+//    @PutMapping("/orders/{id}")
+//    public RedirectView updateOrder(@PathVariable Long id){
+//        orderService.updateStuff(id);
+//        RedirectView redirectView = new RedirectView();
+//        redirectView.setUrl("/allOrdersPage");
+//        return redirectView;
+//    }
+
+    private void setupModel(Model model){
+        List<ReservationDTO> allReservations = reservationService.getAllReservations();
+        model.addAttribute("AllReservations",allReservations);
+        model.addAttribute("allMenus",menuService.getAllMenus());
+        model.addAttribute("allOrders",orderService.getAllOrders());
+    }
+}
+
+
